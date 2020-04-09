4df00a306481d545dbf6cee69ff6be5e43b591fb
Tanya19Lion <tanyatriteknitskaya@gmail.com>
2020-04-09 16:31:36 +0300

    made gallery

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,16 @@
 $(function(){
- 
+    $(".gallery__item-zoom").on("click", function(e){
+        e.preventDefault();
+        // $('.image-popup').magnificPopup({
+        //     type: 'image',
+        //     zoom: {
+        //         enabled: true,
+        //         duration: 300 // продолжительность анимации. Не меняйте данный параметр также и в CSS
+        //     }
+        // });
+
+    });
+     
 
     // new WOW().init();
 
