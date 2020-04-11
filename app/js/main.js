c73a2a2c1b76d32a44ca3c53e3988f81ca7b4d1b
Tanya19Lion <tanyatriteknitskaya@gmail.com>
2020-04-11 18:04:40 +0300

    made adattive

--- app/js/main.js
+++ app/js/main.js
@@ -14,11 +14,28 @@ $(function(){
     $(".journal__content").slick({
         slidesToShow: 2,
         slidesToScroll: 2,
-        nextArrow: '<button type="button" class="slick_btn slick-next"></button>',
-        prevArrow: '<button type="button" class="slick_btn slick-prev"></button>',
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: false,
+        responsive: [
+            {
+              breakpoint: 1031,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                }
+            }
+        ]
+     });
+
+    $(".header__menu-mobile").on("click", function(){
+        $('.header__menu-list').toggleClass("active").slideToggle();
+
+        if ($(".header__menu-list").hasClass("active")) $(".header__info").addClass("movedown");
+        else $(".header__info").removeClass("movedown");
     });
-    // new WOW().init();
 
-
+    new WOW().init();
 });
 
