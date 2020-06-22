ef2e8ebf249ef439809c8330f1baff95de3e9f3b
Tanya19Lion <tanyatriteknitskaya@gmail.com>
2020-06-22 14:04:46 +0300

    added zoom to the pictures

--- app/js/main.js
+++ app/js/main.js
@@ -28,3 +28,53 @@ $(function(){
     new WOW().init();
 });
 
+
+const imgPopup = document.createElement('div'),
+    commonSection = document.querySelector('.gallery'),
+    bigImage = document.createElement('img'),
+    scroll = calcScroll();
+
+imgPopup.classList.add('popup');
+commonSection.appendChild(imgPopup);
+
+imgPopup.style.display = 'none';
+imgPopup.style.justifyContent = 'center';
+imgPopup.style.alignItems = 'center';
+
+imgPopup.appendChild(bigImage);
+
+commonSection.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    target = event.target;
+    console.log(target);
+
+    if (target && target.classList.contains('gallery__preview')) {
+        imgPopup.style.display = 'flex';
+        const path = target.parentNode.getAttribute('href');
+        bigImage.setAttribute('src', path);
+        document.body.style.overflow = 'hidden';
+        document.body.style.marginRight = `${scroll}px`;
+    }
+
+    if (target && target.matches('div .popup')) {
+        imgPopup.style.display = 'none';
+        document.body.style.overflow = '';
+        document.body.style.marginRight = `0px`;
+    }
+});
+
+function calcScroll() {
+    let div = document.createElement('div');
+
+    div.style.width= '50px';
+    div.style.height = '50px';
+    div.style.overflowY = 'scroll';
+    div.style.visibility = 'hidden';
+
+    document.body.appendChild(div);
+    let scrollWidth = div.offsetWidth - div.clientWidth;
+    div.remove();
+
+    return scrollWidth;
+}
